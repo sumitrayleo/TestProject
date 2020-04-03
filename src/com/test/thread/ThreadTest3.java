0f575ee5c4e7d83680ea788f5526e6b864d8358a
Sumit Ray <sumitrayleo@gmail.com>
2020-04-03 13:06:48 +0530

    coverted to Lambda

--- src/com/test/thread/ThreadTest3.java
+++ src/com/test/thread/ThreadTest3.java
@@ -4,9 +4,7 @@ public class ThreadTest3 {
 
     public static void main(String[] args) {
         System.out.println("Start Thread - A");
-        Thread thread = new Thread(new Runnable(){
-            @Override
-            public void run() {
+        Thread thread = new Thread(() -> {
                 for(int i = 0; i < 100; i++){
                     try {
                         Thread.sleep(10);
@@ -15,9 +13,7 @@ public class ThreadTest3 {
                     }
                     System.out.println("Number: " + i + " - " + Thread.currentThread().getName());
                 }
-            }
-            
-        });
+            });
         thread.start();
         
         System.out.println("Start Thread - B");
