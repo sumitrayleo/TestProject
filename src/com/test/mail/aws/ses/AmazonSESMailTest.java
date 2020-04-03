20ec3446422db6926abbc77c359ae2b100467d97
Sumit Ray <sumitrayleo@gmail.com>
2020-04-03 13:01:16 +0530

    Removed keys

--- src/com/test/mail/aws/ses/AmazonSESMailTest.java
+++ src/com/test/mail/aws/ses/AmazonSESMailTest.java
@@ -6,15 +6,15 @@ import javax.mail.internet.*;
 
 public class AmazonSESMailTest {
 
-    static final String FROM = "Sumit.Ray@ehi.com"; // Replace with your "From" address. This address must be verified.
-    static final String TO = "Sumit.Ray@ehi.com"; // Replace with a "To" address. If your account is still in the
+    static final String FROM = ""; // Replace with your "From" address. This address must be verified.
+    static final String TO = ""; // Replace with a "To" address. If your account is still in the
                                                       // sandbox, this address must be verified.
 
     static final String BODY = "This email was sent through the Amazon SES SMTP interface by using Java.";
     static final String SUBJECT = "Amazon SES test";
 
-    static final String SMTP_USERNAME = "AKIAJFUEZYJJXJGZMUHA"; // Replace with your SMTP username.
-    static final String SMTP_PASSWORD = "Ags+Rff2PHUmJXEvodSyU6H0tWRsEpkfb6tC4nsEBumn"; // Replace with your SMTP password.
+    static final String SMTP_USERNAME = ""; // Replace with your SMTP username.
+    static final String SMTP_PASSWORD = ""; // Replace with your SMTP password.
 
     // Amazon SES SMTP host name. 
     static final String HOST = "email-smtp.us-east-1.amazonaws.com";
@@ -37,7 +37,7 @@ public class AmazonSESMailTest {
         msg.setFrom(new InternetAddress(FROM));
         msg.setRecipient(Message.RecipientType.TO, new InternetAddress(TO));
         msg.reply(true);
-        msg.setReplyTo(new javax.mail.Address[] { new javax.mail.internet.InternetAddress("Kuhely.Das@ehi.com"),  new InternetAddress(FROM)});
+        msg.setReplyTo(new javax.mail.Address[] { new javax.mail.internet.InternetAddress(""),  new InternetAddress(FROM)});
         msg.setSubject(SUBJECT);
         msg.setContent(BODY, "text/plain");
 
